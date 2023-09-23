fedb676e6eb1263550f40b431682bc79fefa9854
Aldyytzyy <145819990+Aldyytzyy@users.noreply.github.com>
2023-09-23 09:55:34 +0700

    Create summation.cpp

---
+++ summation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int ak1,ak,tol;
+cout<<"enter number first:";
+cin>>ak;
+cout<<"enter number second:";
+cin>>ak1;
+tol=ak1+ak;
+cout<<"result:"<<tol<<endl;
+return 0;
+}
